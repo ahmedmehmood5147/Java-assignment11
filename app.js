e83bb7e3858c3b674b41e469124b96edf6ff20b5
ahmedmehmood5147 <am9474036@gmail.com>
2023-12-10 06:15:06 +0500

    assignment 11

---
+++ app.js
@@ -0,0 +1,12 @@
+var firstName = "ahmed "
+var secondName = "mehmood"
+var fullName = firstName + secondName
+document.write(fullName+"<br>")
+var userName = prompt("What is your favourite mobile phone model")
+document.write (userName.length)
+var pakisan = "Pakistani"
+document.write(`string ${pakisan}<br>`)
+document.write("index of n is" +pakisan.indexOf("n"))
+var firstName = "ahmed"
+var secondName = "mehmood"
+document.write("<br>"+firstName.concat(secondName))
